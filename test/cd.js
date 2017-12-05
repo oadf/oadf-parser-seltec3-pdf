f6b25076b90ba3f75a9d91a70d64bbe810309b54
DATEC-SCHMIDT\Christian <c.dethloff@datec-schmidt.de>
2017-12-05 15:56:54 +0100

    fix the parser

---
+++ test/cd.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var _oadfParserSeltec3Pdf = require('../lib');
+
+var _oadfParserSeltec3Pdf2 = _interopRequireDefault(_oadfParserSeltec3Pdf);
+
+var _fs = require('fs');
+
+var _fs2 = _interopRequireDefault(_fs);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var filePath = './test/cd4.pdf';
+var data = new Uint8Array(_fs2.default.readFileSync(filePath));
+(0, _oadfParserSeltec3Pdf2.default)(data).then(
+	function (schema) {
+		console.log(schema);
+	}
+);
